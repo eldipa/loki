1f5cd6c5314e1fc81481f8f056d8ca67f9353ad6
Martin Di Paola <martinp.dipaola@gmail.com>
2020-05-01 00:39:37 +0000

    Use pause asm inst for cpu_relay; allow the user to change this

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ CFLAGS = -std=gnu11 -march=core2
 LDLIBS = -lpthread
 INCLUDES = -I.
 
+CONFFLAGS = -DLOKI_CPU_RELAX_INSTR_PAUSE
+
+CFLAGS += $(CONFFLAGS)
+
 ifeq (1,$(DEBUG))
 	CFLAGS += -O0 -ggdb -DLOKI_ENABLE_DEBUG_LOCK -DDEBUG
 else
--- loki/lock.h
+++ loki/lock.h
@@ -2,7 +2,20 @@
 #define LOKI_LOCK_H_
 
 // XXX assumption: we are running on an intel x86 CPU
+// https://c9x.me/x86/html/file_module_x86_id_232.html
+#if defined (LOKI_CPU_RELAX_INSTR_PAUSE)
+#define loki_cpu_relax() do { asm volatile("pause\n": : :"memory"); } while(0)
+
+// XXX assumption: we are running on an intel x86 CPU
 // https://elixir.bootlin.com/linux/v4.5/source/arch/x86/include/asm/processor.h#L560
+#elif defined (LOKI_CPU_RELAX_INSTR_REP_NOP)
 #define loki_cpu_relax() do { asm volatile("rep; nop" ::: "memory"); } while(0)
 
+// Assume that the user provided one impl, fail otherwise
+#else
+#if !defined (loki_cpu_relax)
+#error "No loki_cpu_relax macro was defined. You need to provide one of choose one of the valid options for LOKI_CPU_RELAX_INSTR (predefined loki_cpu_relax macros)"
+#endif
+
+#endif
 #endif
